ddef1ab265a7db58983daf2078dc009269b361ce
Katerina Maneva <k.g.maneva@gmail.com>
2024-02-17 17:16:50 +0200

    Insert values to the tables

--- src/main/resources/database/db.sql
+++ src/main/resources/database/db.sql
@@ -118,3 +118,45 @@ VALUES ('Autospace', 'Honda'),
        ('Car Fixers', NULL),
        ('Wheels Doc', NULL);
 
+INSERT INTO customer (email, first_name, last_name, phone_number)
+VALUES ('ivan.ivanov@gmail.com', 'Ivan', 'Ivanov', '+359123456789'),
+       ('m.petkova@mail.bg', 'Mariya', 'Petkova', '+359987654321'),
+       ('g_georgiev@abv.bg', 'Georgi', 'Georgiev', '+359876543210'),
+       ('stoyanova.elena@gmail.com', 'Elena', 'Stoyanova', '+359558325555'),
+       ('petar.h.dimitrov@mail.bg', 'Petar', 'Dimitrov', '+359123123123');
+
+INSERT INTO employee (email, first_name, last_name, phone_number, working_at)
+VALUES ('ivan-nikolov@gmail.com', 'Ivan', 'Nikolov', '+359888811111', '1'),
+       ('georgiev.g@gmail.com', 'Grigor', 'Georgiev', '+359888822222', '2'),
+       ('p.dimitrov@gmail.com', 'Pavel', 'Dimitrov', '+359888833333', '3'),
+       ('stefanov92@mail.bg', 'Stefan', 'Stefanov', '+359888844444', '3'),
+       ('nikolan@mail.bg', 'Nikola', 'Nikolov', '+359888855555', '1'),
+       ('alexander.hr@abv.bg', 'Alexander', 'Petkov', '+359888866666', '2'),
+       ('petkov@gmail.com', 'Ivan', 'Petkov', '+359888877777', '2');
+
+INSERT INTO car (vin, license_plate, brand, model, year, owner_email)
+VALUES ('123A456B789C01234', 'СА1234АВ', 'Audi', 'A4', '2022', 'ivan.ivanov@gmail.com'),
+       ('456D789E012F34567', 'PB2345EH', 'BMW', 'X5', '2020', 'stoyanova.elena@gmail.com'),
+       ('789G012H345I67890', 'CO3453EE', 'Chevrolet', 'Cruze', '2021', 'petar.h.dimitrov@mail.bg'),
+       ('012J345K678L90123', 'CT4560GH', 'Ford', 'Focus', '2019', 'g_georgiev@abv.bg'),
+       ('345M678N901O23456', 'EB5679HD', 'Honda', 'Civic', '2020', 'm.petkova@mail.bg'),
+       ('678P901Q234R56789', 'CB6278KK', 'Hyundai', 'Elantra', '2022', 'ivan.ivanov@gmail.com');
+
+INSERT INTO appointment (customer_email, car_center_id, car_license_plate, date_created, date_of_appointment,
+                         has_passed)
+VALUES ('ivan.ivanov@gmail.com', 2, 'СА1234АВ', '2022-01-15', '2022-02-01', 1),
+       ('m.petkova@mail.bg', 1, 'EB5679HD', '2024-02-10', '2024-03-05', 0),
+       ('g_georgiev@abv.bg', 3, 'CT4560GH', '2023-03-20', '2023-04-15', 1),
+       ('stoyanova.elena@gmail.com', 2, 'PB2345EH', '2024-03-05', '2024-03-21', 0),
+       ('petar.h.dimitrov@mail.bg', 3, 'CO3453EE', '2024-02-15', '2024-06-10', 0),
+       ('ivan.ivanov@gmail.com', 2, 'CB6278KK', '2024-02-20', '2024-03-25', 0),
+       ('m.petkova@mail.bg', 1, 'EB5679HD', '2023-07-10', '2023-08-05', 1);
+
+INSERT INTO service_job (employee_email, type, date_started, date_finished, price, state, appointment_id)
+VALUES ('ivan-nikolov@gmail.com', 'Brake Repair', '2022-02-01', '2022-02-02', 120.50, 'Completed', 1),
+       ('georgiev.g@gmail.com', 'Oil Change', '2024-03-05', '2024-03-06', 80.00, 'In Progress', 2),
+       ('p.dimitrov@gmail.com', 'Diagnostics', '2023-04-15', '2023-04-16', 150.25, 'Completed', 3),
+       ('stefanov92@mail.bg', 'Wheel Alignment', '2024-02-21', '2024-03-22', 90.75, 'In Progress', 4),
+       ('nikolan@mail.bg', 'Engine Diagnostics', '2024-04-10', NULL, NULL, 'Pending', 5),
+       ('alexander.hr@abv.bg', 'Paint and Body Work', '2024-03-25', NULL, NULL, 'Pending', 6),
+       ('petkov@gmail.com', 'Filter Change', '2023-08-05', '2023-08-06', 65.80, 'Completed', 7);
